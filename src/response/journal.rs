891feb9d2d956e0c24464badce05c12e3781b189
Emil Lindfors <emil.tomson.lindfors@gmail.com>
2024-08-24 09:21:09 +0200

    refactor: Update struct field names in response/journal.rs

--- src/response/journal.rs
+++ src/response/journal.rs
@@ -3,101 +3,101 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 #[serde(rename_all = "kebab-case")]
-struct Counts {
-    total_dois: i32,
-    current_dois: i32,
-    backfile_dois: i32,
+pub struct Counts {
+    pub total_dois: i32,
+    pub current_dois: i32,
+    pub backfile_dois: i32,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 #[serde(rename_all = "kebab-case")]
-struct Coverage {
-    affiliations_current: i32,
-    similarity_checking_current: i32,
-    descriptions_current: i32,
-    ror_ids_current: i32,
-    references_backfie: i32,
-    funders_backfile: i32,
-    licenses_backfile: i32,
-    funders_current: i32,
-    affiliations_backfile: i32,
-    resource_links_backfile: i32,
-    orcids_backfile: i32,
-    update_policies_current: i32,
-    ror_ids_backfile: i32,
-    orcids_current: i32,
-    similarity_checking_backfile: i32,
-    descriptions_backfile: i32,
-    award_numbers_backfile: i32,
-    update_policies_backfile: i32,
-    licenses_current: i32,
-    award_numbers_current: i32,
-    abstracts_backfile: i32,
-    resource_links_current: i32,
-    abstracts_current: i32,
-    references_current: i32,
+pub struct Coverage {
+    pub affiliations_current: i32,
+    pub similarity_checking_current: i32,
+    pub descriptions_current: i32,
+    pub ror_ids_current: i32,
+    pub references_backfie: i32,
+    pub funders_backfile: i32,
+    pub licenses_backfile: i32,
+    pub funders_current: i32,
+    pub affiliations_backfile: i32,
+    pub resource_links_backfile: i32,
+    pub orcids_backfile: i32,
+    pub update_policies_current: i32,
+    pub ror_ids_backfile: i32,
+    pub orcids_current: i32,
+    pub similarity_checking_backfile: i32,
+    pub descriptions_backfile: i32,
+    pub award_numbers_backfile: i32,
+    pub update_policies_backfile: i32,
+    pub licenses_current: i32,
+    pub award_numbers_current: i32,
+    pub abstracts_backfile: i32,
+    pub resource_links_current: i32,
+    pub abstracts_current: i32,
+    pub references_current: i32,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 #[serde(rename_all = "kebab-case")]
-struct Flags {
-    deposits_abstracts_current: bool,
-    deposits_orcids_current: bool,
-    deposits: bool,
-    deposits_affiliations_backfile: bool,
-    deposits_update_policies_backfile: bool,
-    deposits_award_numbers_current: bool,
-    deposits_resource_links_current: bool,
-    deposits_ror_ids_current: bool,
-    deposits_articles: bool,
-    deposits_affiliations_current: bool,
-    deposits_funders_current: bool,
-    deposits_references_backfile: bool,
-    deposits_ror_ids_backfile: bool,
-    deposits_abstracts_backfile: bool,
-    deposits_licenses_backfile: bool,
-    deposits_award_numbers_backfile: bool,
-    deposits_descriptions_current: bool,
-    deposits_references_current: bool,
-    deposits_resource_links_backfile: bool,
-    deposits_descriptions_backfile: bool,
-    deposits_orcids_backfile: bool,
-    deposits_funders_backfile: bool,
-    deposits_update_policies_current: bool,
-    deposits_licenses_current: bool,
+pub struct Flags {
+    pub deposits_abstracts_current: bool,
+    pub deposits_orcids_current: bool,
+    pub deposits: bool,
+    pub deposits_affiliations_backfile: bool,
+    pub deposits_update_policies_backfile: bool,
+    pub deposits_award_numbers_current: bool,
+    pub deposits_resource_links_current: bool,
+    pub deposits_ror_ids_current: bool,
+    pub deposits_articles: bool,
+    pub deposits_affiliations_current: bool,
+    pub deposits_funders_current: bool,
+    pub deposits_references_backfile: bool,
+    pub deposits_ror_ids_backfile: bool,
+    pub deposits_abstracts_backfile: bool,
+    pub deposits_licenses_backfile: bool,
+    pub deposits_award_numbers_backfile: bool,
+    pub deposits_descriptions_current: bool,
+    pub deposits_references_current: bool,
+    pub deposits_resource_links_backfile: bool,
+    pub deposits_descriptions_backfile: bool,
+    pub deposits_orcids_backfile: bool,
+    pub deposits_funders_backfile: bool,
+    pub deposits_update_policies_current: bool,
+    pub deposits_licenses_current: bool,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 #[serde(rename_all = "kebab-case")]
-struct CoverageType {
-    all: Coverage,
-    current: Coverage,
-    backfile: Coverage,
+pub struct CoverageType {
+    pub all: Coverage,
+    pub current: Coverage,
+    pub backfile: Coverage,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 #[serde(rename_all = "kebab-case")]
 pub struct Journal {
-    last_status_check_time: i64,
-    counts: Counts,
-    breakdowns: serde_json::Value, // You can use Value to represent dynamic data
-    publisher: String,
-    coverage: Coverage,
-    title: String,
-    subjects: Vec<String>,
-    coverage_type: CoverageType,
-    flags: Flags,
+    pub last_status_check_time: i64,
+    pub counts: Counts,
+    pub breakdowns: serde_json::Value, // You can use Value to represent dynamic data
+    pub publisher: String,
+    pub coverage: Coverage,
+    pub title: String,
+    pub subjects: Vec<String>,
+    pub coverage_type: CoverageType,
+    pub flags: Flags,
     #[serde(rename = "ISSN")]
-    issn: Vec<String>,
-    issn_type: IssnType,
+    pub issn: Vec<String>,
+    pub issn_type: IssnType,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 #[serde(rename_all = "kebab-case")]
-struct IssnType {
-    value: String,
+pub struct IssnType {
+    pub value: String,
     #[serde(rename = "type")]
-    type_: String, // Renamed to type_ to avoid keyword conflict
+    pub type_: String, // Renamed to type_ to avoid keyword conflict
 }
 
 #[derive(Debug, Deserialize, Serialize)]
